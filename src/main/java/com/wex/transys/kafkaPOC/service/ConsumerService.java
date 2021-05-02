e2aa0f7506796b6eb6cdb8a4b3ed37baae48a16e
W641711 <deepak.velagaleti@wexinc.com>
2021-05-02 15:56:19 -0400

    Formatting

--- src/main/java/com/wex/transys/kafkaPOC/service/ConsumerService.java
+++ src/main/java/com/wex/transys/kafkaPOC/service/ConsumerService.java
@@ -13,28 +13,32 @@ public class ConsumerService
     @Autowired
     SimpMessagingTemplate template;
 
-    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"0"})}, groupId = "group_idA", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"0"})}, groupId = "group_idA",
+            containerFactory = "kafkaListenerContainerFactory")
     public void consumeA0(User message)
     {
         template.convertAndSend("/stream/consumerA0", message);
         System.out.println(String.format("$$$$ => Consumer-A0: %s", message));
     }
 
-    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"1"})}, groupId = "group_idA", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"1"})}, groupId = "group_idA",
+            containerFactory = "kafkaListenerContainerFactory")
     public void consumeA1(User message)
     {
         template.convertAndSend("/stream/consumerA1", message);
         System.out.println(String.format("$$$$ => Consumer-A1: %s", message));
     }
 
-    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"0"})}, groupId = "group_idB", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"0"})}, groupId = "group_idB",
+            containerFactory = "kafkaListenerContainerFactory")
     public void consumeB0(User message)
     {
         template.convertAndSend("/stream/consumerB0", message);
         System.out.println(String.format("$$$$ => Consumer-B0: %s", message));
     }
 
-    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"1"})}, groupId = "group_idB", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topicPartitions = { @TopicPartition(topic = "test", partitions = {"1"})}, groupId = "group_idB",
+            containerFactory = "kafkaListenerContainerFactory")
     public void consumeB1(User message)
     {
         template.convertAndSend("/stream/consumerB1", message);
